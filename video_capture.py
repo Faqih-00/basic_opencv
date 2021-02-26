84680689d10df114cf060b5c81458f702b426e8c
Aether99 <70425237+Faqih-00@users.noreply.github.com>
2021-02-26 21:27:09 +0700

    Update video_capture.py

--- video_capture.py
+++ video_capture.py
@@ -11,12 +11,11 @@ while True:
     face = face_cascade.detectMultiScale(gray)
     for (x, y, w, h) in face:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-        """roi_gray = gray[y:y + h, x:x + w]
+        roi_gray = gray[y:y + h, x:x + w]
         roi_color = frame[y:y + h, x:x + w]
         eye = eye_cascade.detectMultiScale(roi_gray)
         for (ex, ey, ew, eh) in eye:
             cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 250, 0), 2)
-        """
     cv2.imshow("frame", frame)
     if cv2.waitKey(1) == ord('q'):
         break
