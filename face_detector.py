3caa76caf2886db61a9f922a578bd1c6a0d2930d
Aether99 <70425237+Faqih-00@users.noreply.github.com>
2021-02-26 21:47:16 +0700

    Update face_detector.py

--- face_detector.py
+++ face_detector.py
@@ -5,7 +5,7 @@ import numpy as np
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-img = cv2.resize(cv2.imread('assets/try.jpg'), (0, 0), fx=.75, fy=.75)
+img = cv2.resize(cv2.imread('assets/eminem.jpg'), (0, 0), fx=.75, fy=.75)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 face = face_cascade.detectMultiScale(gray, 1.3, 5)
@@ -14,8 +14,8 @@ for (x, y, w, h) in face:
     roi_gray = gray[y:y+h, x:x+w]
     roi_color = img[y:y+h, x:x+w]
     eye = eye_cascade.detectMultiScale(roi_gray)
-    for (ex, ey, ew, eh) in eye:
-        cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 250, 0), 2)
+    #for (ex, ey, ew, eh) in eye:
+    #    cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 250, 0), 2)
 
 cv2.imshow("Face", img)
 cv2.waitKey(0)
